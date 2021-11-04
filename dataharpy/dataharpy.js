3d876255ea56ac7796da45a4232a975818985a6a
Isabella <degen.isabella@gmail.com>
2021-11-04 09:27:50 +0000

    added hello world button for Data HarPY, also disabled var inspector from showing

--- dataharpy/dataharpy.js
+++ dataharpy/dataharpy.js
@@ -4,10 +4,24 @@ define([
     Jupyter
 ) {
     function load_ipython_extension() {
-        console.log(
-            '[DataHarPy] This is the current notebook application instance:',
-            Jupyter.notebook
-        );
+        console.log('[DataHarPy] Loading extension with current notebook:', Jupyter.notebook);
+
+        var show_data_harpy = function () {
+            console.log("[DataHarPY] Show button clicked")
+            alert('Hello World!');
+        };
+
+        var data_harpy_button = {
+            icon: 'fa-bar-chart', // a font-awesome icon
+            help    : 'Show Data HarPY',
+            help_index : 'zz',
+            handler : show_data_harpy
+        };
+        var prefix = 'dataharpy';
+        var action_name = 'show-data-harpy';
+
+        var full_action_name = Jupyter.keyboard_manager.actions.register(data_harpy_button, action_name, prefix);
+        Jupyter.toolbar.add_buttons_group([full_action_name]);
     }
 
     return {
