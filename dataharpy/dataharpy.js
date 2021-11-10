c2eaef57f10834b5abee779c79f728b309a9c632
Phillip Sloan <phillip.sloan@bristol.ac.uk>
2021-11-10 13:27:09 +0000

    Phil: Removing redundant buttons from dataharpy.js

--- dataharpy/dataharpy.js
+++ dataharpy/dataharpy.js
@@ -8,14 +8,13 @@ define([
 
         var show_data_harpy = function () {
 
-            Jupyter.notebook.insert_cell_below('code').set_text("from sys import getsizeof\nfrom IPython import get_ipython\nfrom IPython.core.magics.namespace import NamespaceMagics\nimport ipywidgets as widgets\nimport pandas as pd\n_nms = NamespaceMagics()\n_Jupyter = get_ipython()\n_nms.shell = _Jupyter.kernel.shell\nvalues = _nms.who_ls()\nlocals = []\nfor v in values:\n\tif type(eval(v)).__name__ == 'DataFrame':\n\t\tlocals.append(v)\ndropdown = widgets.Dropdown(options=locals,vdescription='Number:', disabled=False)\nfrom backend_widgets import analyser\nimport numpy as np\nanalyser_btn = widgets.Button(description='Create Analyser')\ndef analyser_btn_eventhandler(obj):\n\tglobal analyse\n\tanalyse = analyser(eval(dropdown.value), 'target')\nanalyser_btn.on_click(analyser_btn_eventhandler)\nbtn1 = widgets.Button(description='Classification')\nbtn2 = widgets.Button(description='Regression')\ndef btn1_eventhandler(obj):\n\tprint('Hello from the {} button!'.format(obj.description))\ndef btn2_eventhandler(obj):\n\tprint('Hello from the {} button!'.format(obj.description))\nbtn1.on_click(btn1_eventhandler)\nbtn2.on_click(btn2_eventhandler)")
+            Jupyter.notebook.insert_cell_below('code').set_text("from sys import getsizeof\nfrom IPython import get_ipython\nfrom IPython.core.magics.namespace import NamespaceMagics\nimport ipywidgets as widgets\nimport pandas as pd\n_nms = NamespaceMagics()\n_Jupyter = get_ipython()\n_nms.shell = _Jupyter.kernel.shell\nvalues = _nms.who_ls()\nlocals = []\nfor v in values:\n\tif type(eval(v)).__name__ == 'DataFrame':\n\t\tlocals.append(v)\ndropdown = widgets.Dropdown(options=locals,vdescription='Number:', disabled=False)\nfrom backend_widgets import analyser\nimport numpy as np\nanalyser_btn = widgets.Button(description='Analyse Data')\ndef analyser_btn_eventhandler(obj):\n\tglobal analyse\n\tanalyse = analyser(eval(dropdown.value), 'target')\nanalyser_btn.on_click(analyser_btn_eventhandler)")
             Jupyter.notebook.select_next()
             Jupyter.notebook.execute_cell()
-            Jupyter.notebook.get_selected_cell().set_text("display(dropdown)\ndisplay(analyser_btn)\ndisplay(btn1)\ndisplay(btn2)")
+            Jupyter.notebook.get_selected_cell().set_text("display(dropdown)\ndisplay(analyser_btn)")
             Jupyter.notebook.execute_cell()
-            Jupyter.notebook.get_selected_cell().set_text("# Select a DataFrame to Compute the Data with:")
+            Jupyter.notebook.get_selected_cell().set_text("# Select a DataFrame to analyse:")
         };
-
 
 
         var data_harpy_button = {
